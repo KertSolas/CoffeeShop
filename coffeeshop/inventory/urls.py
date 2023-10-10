9e6a71767c4f1adb2311657133edbd4fba1a5ec9
KewlWhip <78395423+KertSolas@users.noreply.github.com>
2023-10-10 16:58:25 +0800

    Fixed navbar

--- coffeeshop/inventory/urls.py
+++ coffeeshop/inventory/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib import admin
-from .views import coffeeList, AddCoffee, DeleteCoffee
+from .views import coffeeList, AddCoffee, DeleteCoffee, UpdateCoffee
 
 urlpatterns = [
     path("", coffeeList.as_view(), name="coffeeNames"),
     path("addCoffee/", AddCoffee.as_view(), name="addCoffee"),
     path("deleteCoffee/<int:pk>/", DeleteCoffee.as_view(), name="deleteCoffee"),
+    path("updateCoffee/<int:pk>/", UpdateCoffee.as_view(), name="updateCoffee"),
     path('admin/', admin.site.urls,)
 ]
--- coffeeshop/inventory/views.py
+++ coffeeshop/inventory/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import *
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, DeleteView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import CoffeeName
 # Create your views here.
 
@@ -21,3 +21,11 @@ class DeleteCoffee(DeleteView):
     template_name = "inventory/deleteCoffee.html"
     context_object_name = "coffee"
     success_url = reverse_lazy('coffeeNames')
+
+class UpdateCoffee(UpdateView): 
+    model = CoffeeName
+    template_name = "inventory/updateCoffee.html"
+    fields = '__all__'
+    context_object_name = "coffee"
+    success_url = reverse_lazy('coffeeNames')
+    
